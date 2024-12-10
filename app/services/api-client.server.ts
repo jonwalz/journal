683c03c8e9bae762764fce89e19a12f4f4de8bc4
jonwalz <jon.walz.music@gmail.com>
2024-12-10 22:11:58 +0100

    fix: use api url from env

--- app/services/api-client.server.ts
+++ app/services/api-client.server.ts
@@ -1,6 +1,6 @@
 import { getSession } from "./session.server";
 
-const API_BASE_URL = "http://localhost:3030";
+const API_BASE_URL = process.env.API_URL || "http://localhost:3030";
 
 export interface RequestOptions extends RequestInit {
   data?: unknown;
